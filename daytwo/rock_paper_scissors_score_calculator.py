0a6efaea851a7d8798e5a8b99105e6531f09b6d1
UjuE <UjuE@users.noreply.github.com>
2022-12-02 11:15:29 +0000

    Day 2

--- daytwo/rock_paper_scissors_score_calculator.py
+++ daytwo/rock_paper_scissors_score_calculator.py
@@ -6,6 +6,8 @@ rock = "ROCK"
 paper = "PAPER"
 scissors = "SCISSORS"
 
+play_shapes = [rock, paper, scissors]
+
 outcome_scores = {win: 6, draw: 3, lose: 0}
 play_shape_scores = {rock: 1, paper: 2, scissors: 3}
 
@@ -26,10 +28,10 @@ expected_outcomes = {
 
 
 class Rule:
-    def __init__(self, play: str, beats: str, looses_to:str):
+    def __init__(self, play: str, beats: str):
         self.play = play
         self.beats = beats
-        self.looses_to = looses_to
+        self.looses_to = list(filter(lambda x: x not in [play, beats], play_shapes))[0]
 
     def outcome_with_opponent_play(self, opponent_play):
         if self.play == opponent_play:
@@ -48,41 +50,38 @@ class Rule:
             return self.looses_to
 
 
-rules = {rock: Rule(rock, scissors, paper),
-         scissors: Rule(scissors, paper, rock),
-         paper: Rule(paper, rock, scissors)}
+rules = {rock: Rule(rock, beats=scissors),
+         scissors: Rule(scissors, beats=paper),
+         paper: Rule(paper, beats=rock)}
 
 
-def calculate_based_on_play_shape(plays_line: str):
-    plays = plays_line.split(" ")
-    opponent_play = play_shape_dictionary.get(plays[0])
-    your_play = play_shape_dictionary.get(plays[1])
+def calculate_score(your_play, opponent_play):
     outcome = rules.get(your_play).outcome_with_opponent_play(opponent_play)
     return play_shape_scores.get(your_play) + outcome_scores.get(outcome)
 
 
-def calculate_based_on_outcome(plays_line: str):
-    plays = plays_line.split(" ")
+def calculate_based_on_play_shape(plays):
+    opponent_play = play_shape_dictionary.get(plays[0])
+    your_play = play_shape_dictionary.get(plays[1])
+    return calculate_score(your_play, opponent_play)
+
+
+def calculate_based_on_outcome(plays):
     opponent_play = play_shape_dictionary.get(plays[0])
     expected_outcome = expected_outcomes.get(plays[1])
     your_play = rules.get(opponent_play).outcome_shape(expected_outcome)
-    return play_shape_scores.get(your_play) + outcome_scores.get(expected_outcome)
+    return calculate_score(your_play, opponent_play)
 
 
-def calculate_total(plays_strategy: str):
-    return sum(map(lambda play_round: calculate_based_on_play_shape(play_round), list(plays_strategy.splitlines())))
-
-
-def calculate_total_2(plays_strategy: str):
-    return sum(map(lambda play_round: calculate_based_on_outcome(play_round), list(plays_strategy.splitlines())))
+def calculate_total_score(plays_strategy, func):
+    return sum(map(lambda play_round: func(play_round.split(" ")), list(plays_strategy.splitlines())))
 
 
 def use_input():
     with open("test_input_data.txt", "r") as input_file:
         input_data = input_file.read()
-        total_score = calculate_total(input_data)
-        print("Answer 1.", total_score)
-        print("Answer 2.", calculate_total_2(input_data))
+        print("Answer 1.", calculate_total_score(input_data, calculate_based_on_play_shape))
+        print("Answer 2.", calculate_total_score(input_data, calculate_based_on_outcome))
 
 
 use_input()
